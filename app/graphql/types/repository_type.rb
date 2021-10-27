95eb16885ced485ed6ad65c46cd0ec017f3f4b44
Priscila Aranha <aranhaqg@gmail.com>
2021-10-27 11:20:11 -0300

    Add graphql and repository model

---
+++ app/graphql/types/repository_type.rb
@@ -0,0 +1,9 @@
+module Types
+  class RepositoryType < Types::BaseObject
+    field :id, ID, null: false
+    field :name, String, null: false
+    field :full_name, String, null: true
+    field :description, String, null: true
+    field :url, String, null: true
+  end
+end
---
+++ app/models/repository.rb
@@ -0,0 +1,12 @@
+class Repository
+  attr_accessor :id, :name, :full_name, :description, :url
+  
+  def initialize(id, name, full_name, description, url)
+    @id = id
+    @name = name
+    @full_name = full_name
+    @description = description
+    @url = @url
+  end
+  
+end
